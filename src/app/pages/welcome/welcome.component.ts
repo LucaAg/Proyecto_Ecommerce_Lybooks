32918a948272ff7491a3d99afc55402241557a5f
Luca <nahuquilmes2000@gmail.com>
2023-11-15 21:23:33 -0300

    Random images in Welcome page

--- src/app/pages/welcome/welcome.component.ts
+++ src/app/pages/welcome/welcome.component.ts
@@ -8,9 +8,41 @@ import { ApiBookService } from 'src/app/services/api-book.service';
   styleUrls: ['./welcome.component.css']
 })
 export class WelcomeComponent {
+  imagesArray:any = [];
+  imagesArray2:any = [];
+  randomNumbers:any[]=[];
 
   constructor(private apiBook:ApiBookService)
   {
     console.log(apiBook.fetchData());
+    this.fillArrayImage();
+  }
+
+  fillArrayImage()
+  {
+    let randomNumber = 0; 
+    for(let i = 0; i < 12;i++)
+    {
+      randomNumber = Math.floor(Math.random() * (16)) + 1; 
+      if(!this.randomNumbers.includes(randomNumber))
+      {
+        this.randomNumbers.push(randomNumber);
+        const image = new Image();
+        if(this.imagesArray.length != 6)
+        {
+          image.src = "../../../assets/Welcome/" + randomNumber + ".jpg";
+          this.imagesArray.push(image);
+        }
+        else
+        {
+          image.src = "../../../assets/Welcome/" + randomNumber + ".jpg";
+          this.imagesArray2.push(image);
+        }
+      }
+      else
+      {
+        i--;
+      }
+    }
   }
 }
